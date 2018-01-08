6ee62b8df9f7aeb7bad27eb6107a17b8881efd45
Marvin Clerge <clergemarvin@gmail.com>
2018-01-08 17:14:14 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+function printBadges(array) {
+  for (var i = 0; i < array.length; i++) {
+    console.log(`Welcome ${array[i]}! You are employee #${i+1}.`);
+  }
+  return array;
+}
+
+function flipCoin() {
+  return Math.random() >= 0.5
+}
+
+function tailsNeverFails() {
+  counter = 0;
+  while (flipCoin()) {
+    counter++;
+    console.log(`${counter} tails`);
+  }
+  return `You got ${counter} tails in a row!`;
+}
